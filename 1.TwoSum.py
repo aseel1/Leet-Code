d786e917e63ed45be99a6173d3424e53a12eaa10
Aseel.shaheen <aseel.sh@informatika.co.il>
2025-05-31 15:57:11 +0300

    small fix.

--- 1.TwoSum.py
+++ 1.TwoSum.py
@@ -1,7 +1,7 @@
 from typing import List
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target) :
         # Dictionary to store (number → index)
         index_map = {}
 
